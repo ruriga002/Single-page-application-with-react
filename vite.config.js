b9788367c8be355a7b8392d9ae690f4eedf4e91b
ruriga002 <eruriga002@gmail.com>
2026-05-04 01:42:19 +0300

    fix deployment base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // FIXED: must match your repo name exactly
+  base: "/Single-page-application-with-react/",
+
   test: {
     globals: true,
     environment: "jsdom"
